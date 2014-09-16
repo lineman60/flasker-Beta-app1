297aab2e9db9532513f1f8e02dee6a96d41db4d1
Beryl <bsnyder@nucleoware.com>
2014-09-15 23:16:59 -0500

    pg125

---
+++ config.py
@@ -0,0 +1,21 @@
+__author__ = 'Beryl'
+#config.py
+# yes this should be in git ignore, tutorial app don't care.
+
+
+import os
+
+#graps the folder, yay OS agnostic
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+#Defines the full path
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+
