3bc245277bc4e92a81784ed0376e8688156db0cd
Beryl <bsnyder@nucleoware.com>
2014-09-21 22:35:32 -0500

    Chapter 10 pg 155 Removed DB close line and that seemed to fix the SQLAlchemy issue, Now Getting a Date Type issue,  formatting?

--- db_create.py
+++ db_create.py
@@ -6,24 +6,6 @@ from datetime import date
 
 
 db.create_all()
-db.session.add(Task("Finish this tutorial", "09/16/2015", 10, 1))
-db.session.add(Task("Finish Real Python Course 2", "09/17/2015", 10, 1))
+db.session.add(Task("Finish this tutorial", date(2014, 3, 13), 10, 1))
+db.session.add(Task("Finish Real Python Course 2", date(2014, 3, 13), 10, 1))
 db.session.commit()
-
-#import sqlite3
-
-#from config import DATABASE_PATH
-
-#with sqlite3.connect(DATABASE_PATH) as connection:
-    #get a cursor object used to execute SQL commands
-#    c = connection.cursor()
-
-    #create the table
- #   c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#    name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER
-#    NOT NULL)""")
-
-    #Insert dummy data into the table
-
-#    c.execute('INSERT INTO tasks (name, due_date, priority, status)''VALUES("Finish this tutorial", "09/16/2015", 10, 1)')
-#    c.execute('INSERT INTO tasks (name, due_date, priority, status)''VALUES("Finsh Real Python Course 2", "09/17/2015", 10, 1)')
--- views.py
+++ views.py
@@ -51,8 +51,8 @@ def login():
 @login_required
 def tasks():
     open_tasks = db.session.query(Task).filter_by(status='1').order_by(Task.due_date.asc())
-    close_tasks = db.session.query(Task).filter_by(satus='0').order_by(Task.due_date.asc())
-    return render_template('tasks.html', form=AddTaskForm(request.form), open_tasks=open_tasks, close_tasks=close_tasks)
+    closed_tasks = db.session.query(Task).filter_by(status='0').order_by(Task.due_date.asc())
+    return render_template('tasks.html', form=AddTaskForm(request.form), open_tasks=open_tasks, closed_tasks=closed_tasks)
 
 #add new Tasks:
 @app.route('/add/', methods=['GET', 'POST'])
@@ -67,18 +67,38 @@ def new_task():
                 form.priority.data,
                 '1'
             )
-
-        db.session.add(new_task)
-        db.session.commit()
-        flash('new entery was successfully posted. Thanks')
-        return redirect(url_for('tasks'))
+            db.session.add(new_task)
+            db.session.commit()
+            flash('New entry was successfully posted. Thanks.')
+    return redirect(url_for('tasks'))
+# @login_required
+# def new_task():
+#     import datetime
+#     error = None
+#     form = AddTaskForm(request.form)
+#     if request.method == 'POST':
+#         if form.validate_on_submit():
+#             new_task = Task(
+#                 form.name.data,
+#                 form.due_date.data,
+#                 form.priority.data,
+#                 '1')
+#             db.session.add(new_task)
+#             db.session.commit()
+#             flash('new entery was successfully posted. Thanks')
+#             print(new_task)
+#             return redirect(url_for('tasks'))
+#         else:
+#             print('else')
+#             return redirect(url_for('tasks'))
 
 # Mark tasks as complete:
 @app.route('/complete/<int:task_id>/',)
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status":"0"})
+    db.session.query(Task).filter_by(task_id=new_id).update({"status": "0"})
+    db.session.commit()
     flash('the task was marked as complete. ')
     return redirect(url_for('tasks'))
 
@@ -90,6 +110,6 @@ def complete(task_id):
 def delete_entry(task_id):
     new_id = task_id
     db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.commit()
+    db.session.commit()
     flash('the task was deleted.')
     return redirect(url_for('tasks'))
